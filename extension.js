e3812d5bf809c1314a42c5e64bb58d147246a578
Guoliang <guoliang.li@assurly.com>
2023-03-02 22:35:15 +0100

    formatter register

--- extension.js
+++ extension.js
@@ -1,28 +1,50 @@
 /**
  * @file vue-format 入口文件
  */
-const vscode = require('vscode');
-const formatInit = require('./src');
+const vscode = require('vscode')
+const formatInit = require('./src')
+
+function runFormatter(document, range, options) {
+  let acEditor = vscode.window.activeTextEditor
+
+  if (acEditor && acEditor.document.languageId === 'vue') {
+    formatInit.init()
+  } else {
+    vscode.window.showInformationMessage('It‘s not a .vue file')
+  }
+  // vscode.window.showInformationMessage('vue format now');
+}
 
 function activate(context) {
-    // 注册命令，该命令和package.json中命令一致
-    let disposable = vscode.commands.registerCommand('extension.vueFormat', function () {
-        let acEditor = vscode.window.activeTextEditor;
-
-        if (acEditor && acEditor.document.languageId === 'vue') {
-            formatInit.init();
-        } else {
-            vscode.window.showInformationMessage('It‘s not a .vue file');
-        }
-        // vscode.window.showInformationMessage('vue format now');
-    });
-
-    context.subscriptions.push(disposable);
+  // 注册 Formatter 命令
+  context.subscriptions.push(
+    vscode.languages.registerDocumentFormattingEditProvider('vue', {
+      provideDocumentFormattingEdits: (document, options, token) => {
+        return runFormatter(document, null, options)
+      },
+    })
+  )
 }
-exports.activate = activate;
+
+// function activate(context) {
+//     // 注册命令，该命令和package.json中命令一致
+//     let disposable = vscode.commands.registerCommand('extension.vueFormat', function () {
+//         let acEditor = vscode.window.activeTextEditor;
+
+//         if (acEditor && acEditor.document.languageId === 'vue') {
+//             formatInit.init();
+//         } else {
+//             vscode.window.showInformationMessage('It‘s not a .vue file');
+//         }
+//         // vscode.window.showInformationMessage('vue format now');
+//     });
+
+//     context.subscriptions.push(disposable);
+// }
+exports.activate = activate
 
 function deactivate() {
-    vscode.window.showInformationMessage('deactivated');
+  vscode.window.showInformationMessage('deactivated')
 }
 
-exports.deactivate = deactivate;
+exports.deactivate = deactivate
